cdcab4129f2d2efb9941b93ebb0736a1d36f21c9
Habeeb <habeebmaciver@gmail.com>
2024-01-19 08:52:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        name: "Member",
+        label: "Member",
+        icon: "fa fa-users",
+        path: "/skills/member",
+        component: Member,
+        layout: "/admin"
+    };
+}
